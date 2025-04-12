174e10e7421ba4bb4ea21c66d5355deaa6856869
Sheikh Jabed <jabedbd2295@gmail.com>
2025-04-12 22:58:40 +0600

    Next Level JS Repo Added

---
+++ index.js
@@ -0,0 +1,32 @@
+let day = 1;
+let dayName;
+
+switch (day) {
+    case 1:
+coreName = "Friday"
+        break;
+
+    case 2:
+coreName = "Friday"
+        break;
+    case 3:
+coreName = "Friday"
+        break;
+    case 4:
+coreName = "Friday"
+        break;
+    case 5:
+coreName = "Friday"
+        break;
+    case 6:
+coreName = "Friday"
+        break;
+    case 7:
+coreName = "Friday"
+        break;
+
+    default:
+        break;
+}
+
+console.log(dayName);
