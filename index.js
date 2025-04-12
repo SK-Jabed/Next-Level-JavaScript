a34d0da95e8dd7a00f0b36729cc9dc559bbe63b1
Sheikh Jabed <jabedbd2295@gmail.com>
2025-04-12 23:06:38 +0600

    New fuknction and method added

--- index.js
+++ index.js
@@ -2,31 +2,36 @@ let day = 5;
 let dayName;
 
 switch (day) {
-    case 1:
-coreName = "Friday"
-        break;
+  case 1:
+    dayName = "Saturday";
+    break;
 
-    case 2:
-coreName = "Friday"
-        break;
-    case 3:
-coreName = "Friday"
-        break;
-    case 4:
-coreName = "Friday"
-        break;
-    case 5:
-coreName = "Friday"
-        break;
-    case 6:
-coreName = "Friday"
-        break;
-    case 7:
-coreName = "Friday"
-        break;
+  case 2:
+    dayName = "Sunday";
+    break;
 
-    default:
-        break;
+  case 3:
+    dayName = "Monday";
+    break;
+
+  case 4:
+    dayName = "Tuesday";
+    break;
+
+  case 5:
+    dayName = "Wednesday";
+    break;
+
+  case 6:
+    dayName = "Thursday";
+    break;
+
+  case 7:
+    dayName = "Friday";
+    break;
+
+  default:
+    break;
 }
 
 console.log(dayName);
